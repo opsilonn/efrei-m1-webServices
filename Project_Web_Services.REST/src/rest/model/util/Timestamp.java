e0aa0fd51a1b99862d8227be3d39b2943e48c03e
DulcheE <34923261+DulcheE@users.noreply.github.com>
2019-11-24 18:09:05 +0100

    update Date/Timestamp

--- Project_Web_Services.REST/src/rest/model/util/Timestamp.java
+++ Project_Web_Services.REST/src/rest/model/util/Timestamp.java
@@ -42,6 +42,8 @@ public class Timestamp {
 		this.hour = hour;
 		this.minute = minute;
 		this.second = second;
+		
+		resetValue();
 	}
 	
 	
@@ -58,7 +60,7 @@ public class Timestamp {
 	 */
 	public void setYear(int year) {
 		this.year = year;
-		this.setValue();
+		this.resetValue();
 	}
 	/**
 	 * @return the month
@@ -72,7 +74,7 @@ public class Timestamp {
 	 */
 	public void setMonth(int month) {
 		this.month = month;
-		this.setValue();
+		this.resetValue();
 	}
 	/**
 	 * @return the day
@@ -86,7 +88,7 @@ public class Timestamp {
 	 */
 	public void setDay(int day) {
 		this.day = day;
-		this.setValue();
+		this.resetValue();
 	}
 	/**
 	 * @return the hour
@@ -100,7 +102,7 @@ public class Timestamp {
 	 */
 	public void setHour(int hour) {
 		this.hour = hour;
-		this.setValue();
+		this.resetValue();
 	}
 	/**
 	 * @return the minute
@@ -114,7 +116,7 @@ public class Timestamp {
 	 */
 	public void setMinute(int minute) {
 		this.minute = minute;
-		this.setValue();
+		this.resetValue();
 	}
 	/**
 	 * @return the second
@@ -128,20 +130,39 @@ public class Timestamp {
 	 */
 	public void setSecond(int second) {
 		this.second = second;
-		this.setValue();
+		this.resetValue();
 	}
 
 	/**
 	 * @return the value
 	 */
 	public String getValue() {
+		resetValue();
 		return value;
 	}
 
 	/**
 	 * @param value the value to set
 	 */
-	public void setValue() {
+	public void setValue(String value) {
+		String spliter[] = value.split("-");
+
+		this.year = Integer.valueOf(spliter[0]);
+		this.month = Integer.valueOf(spliter[1]);
+
+		spliter = spliter[2].split(" ");
+
+		this.day = Integer.valueOf(spliter[0]);
+
+		spliter = spliter[1].split(":");
+
+		this.hour = Integer.valueOf(spliter[0]);
+		this.minute = Integer.valueOf(spliter[1]);
+		this.second = Integer.valueOf(spliter[2]);
+	}
+	
+	
+	public void resetValue(){
 		this.value = year + "-" + month + "-" + day + " " + hour + ":" + minute
 				+ ":" + second;
 	}
@@ -151,6 +172,7 @@ public class Timestamp {
 	 */
 	@Override
 	public String toString() {
+		resetValue();
 		return value;
 	}
 	
